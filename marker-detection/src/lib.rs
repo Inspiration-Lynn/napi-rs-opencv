b456ed36a64935e1ef1b9de451122960f6f5e73b
lynn <1135464674@qq.com>
2022-05-05 17:00:57 +0800

    疑似解决iterator问题

--- marker-detection/src/lib.rs
+++ marker-detection/src/lib.rs
@@ -6,6 +6,7 @@ use opencv::core::*;
 use opencv::imgcodecs::*;
 use opencv::aruco::*;
 use opencv::types::*;
+use opencv::calib3d::*;
 
 
 #[napi]
@@ -95,43 +96,54 @@ impl OpenCv {
   #[napi]
   pub fn detect_markers(&self, env: Env, filename_in: String, filename_out: String, camera_params: String) {
     // marker detection
-    let image = imread(&filename_in, 1).expect("imread fail");
+    let image = imread(&filename_in, IMREAD_COLOR).expect("imread fail");
     let dictionary: Ptr<Dictionary> = get_predefined_dictionary_i32(DICT_6X6_250).expect("get_predefined_dictionary fail");
     let mut corners: VectorOfVectorOfPoint2f = VectorOfVectorOfPoint2f::default();
     let mut ids: VectorOfi32 = VectorOfi32::default();
     let parameters: Ptr<DetectorParameters> = DetectorParameters::create().expect("DetectorParameters::create fail");
     let mut rejected_img_points: VectorOfVectorOfPoint2f = VectorOfVectorOfPoint2f::default();
-
     detect_markers(&image, &dictionary, &mut corners, &mut ids, &parameters, &mut rejected_img_points, &no_array(), &no_array()).expect("detect_marker fail");
     // println!("corners: {:?}", corners);
-
-    let mut clone_img: Mat = opencv::core::Mat::clone(&image);
-    let border_color: Scalar = Scalar::new(0.0, 255.0, 0.0, 0.0);
-    draw_detected_markers(&mut clone_img, &corners, &ids, border_color).expect("draw_detected_markers fail");
-    imwrite(&filename_out.to_string(), &clone_img, &Vector::new()).expect("imwrite fail");
-
+    
     // read camera parameters from yml/yaml
     let mut camera_matrix: Mat = Mat::default();
     let mut dist_coeffs: Mat = Mat::default();
-    // let fs: FileStorage = FileStorage::new(&camera_params, 0, &String::new()).expect("FileStorage::new fail");
-    // camera_matrix = fs.get_node("camera_matrix").expect("get_node fail").mat().expect("mat fail");
-    // dist_coeffs = fs.get_node("distortion_coefficients").expect("get_node fail").mat().expect("mat fail");
-    // println!("camera_matrix: {:?}", camera_matrix);
-    // println!("dist_coeffs: {:?}", dist_coeffs);
     (camera_matrix, dist_coeffs) = read_camera_parameters(camera_params);
 
-    // pose estimation
+    let mut image_copy: Mat = opencv::core::Mat::clone(&image);
+    let border_color: Scalar = Scalar::new(0.0, 255.0, 0.0, 0.0);
+
     // if at least one marker detected
     if ids.len() > 0 {
+      draw_detected_markers(&mut image_copy, &corners, &ids, border_color).expect("draw_detected_markers fail");
+      // imwrite(&filename_out.to_string(), &image_copy, &Vector::new()).expect("imwrite fail");
+
+      // pose estimation
       let mut rvecs: VectorOfPoint3d = VectorOfPoint3d::default();
       let mut tvecs: VectorOfPoint3d = VectorOfPoint3d::default();
       estimate_pose_single_markers(&corners, 0.05, &camera_matrix, &dist_coeffs, &mut rvecs, &mut tvecs, &mut no_array());
-      println!("rvecs: {:?}", rvecs);
-      println!("tvecs: {:?}", tvecs);
+      // println!("rvecs: {:?}", rvecs);
+      // println!("tvecs: {:?}", tvecs);
 
-      let image2 = imread(&filename_in, IMREAD_GRAYSCALE).expect("imread fail");
-      let mut clone_img: Mat = opencv::core::Mat::clone(&image2);
-      // println!("len: {:?}", ids.len());
+      // draw axis for each marker
+      println!("1");
+      let mut rvecs_iter = rvecs.iter();
+      let mut tvecs_iter = tvecs.iter();
+      println!("2");
+      for i in 0..ids.len() {
+        println!("loop\n");
+        // let rvec: Point3d = rvecs_iter;
+        // let tvec: Point3d = tvecs_iter;
+        println!("{} ----- rvec: {:?} \n tvec: {:?}", i, rvecs_iter.next(), tvecs_iter.next());
+        // draw_frame_axes(&mut image_copy, &camera_matrix, &dist_coeffs, &rvec, &tvec, 0.1, 3);
+        // rvecs_iter.next().unwrap();
+        // tvecs_iter.next().unwrap();
+      }
+
+      // imwrite(&filename_out.to_string(), &image_copy, &Vector::new()).expect("imwrite fail");
+    }
+    else {
+      println!("No marker detected");
     }
 
 
